77755e493ee8fc5d65e271a5f84391835bde0403
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2025-09-17 00:49:28 +0700

    adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 );
