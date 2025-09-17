35cce439b7e5472cdf064f6d13e37f6079f089f0
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2025-09-17 09:46:10 +0700

    adding speed Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx.tsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
       <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>
 );
